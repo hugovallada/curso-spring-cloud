29c716c80a0d372296b4ee6d02097f8786001c4b
hugovallada <valladahugo@gmail.com>
2021-08-27 07:02:29 -0300

    Adiciona o retry ao projeto book-service

--- book-service/src/main/java/com/github/hugovallada/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/github/hugovallada/bookservice/controller/FooBarController.java
@@ -1,16 +1,24 @@
 package com.github.hugovallada.bookservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@Log4j2
 @RestController
-@RequestMapping("/bar")
+@RequestMapping("/books")
 public class FooBarController {
 
+
     @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar")
     public String fooBar(){
-        return "Foo-Bar";
+        log.info("Request to foo-bar is received");
+        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return response.getBody();
     }
 
 
