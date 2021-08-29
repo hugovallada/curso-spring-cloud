dca4eb9f35f4980ad981d45f36ed5d7b3c552b83
hugovallada <valladahugo@gmail.com>
2021-08-29 13:50:22 -0300

    Adiciona o OpenAPI para o book service

--- book-service/src/main/java/com/github/hugovallada/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/github/hugovallada/bookservice/controller/BookController.java
@@ -3,6 +3,8 @@ package com.github.hugovallada.bookservice.controller;
 import com.github.hugovallada.bookservice.api.proxy.CambioProxy;
 import com.github.hugovallada.bookservice.model.Book;
 import com.github.hugovallada.bookservice.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("/books")
 @Log4j2
@@ -29,6 +29,7 @@ public class BookController {
     @Autowired
     private CambioProxy cambioProxy;
 
+    @Operation(summary = "Find specific book by id")
     @SneakyThrows
     @GetMapping("{id}/{currency}")
     public Book getBook(
@@ -39,7 +40,7 @@ public class BookController {
 
         var book = repository.getById(id);
 
-        if(book == null) throw new Exception("Book can't be null");
+        if (book == null) throw new Exception("Book can't be null");
 
         var cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
 
--- book-service/src/main/java/com/github/hugovallada/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/github/hugovallada/bookservice/controller/FooBarController.java
@@ -1,34 +1,33 @@
 package com.github.hugovallada.bookservice.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo bar")
 @Log4j2
 @RestController
 @RequestMapping("/books")
 public class FooBarController {
 
-
+    @Operation(summary = "Foo-bar")
     @GetMapping("/foo-bar")
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") # tenta x numeros de vezes antes de ir pro fallback
     //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") # tenta chamar, mas se tiver muitos acessos ele passa a desviar o fluxo pro fallback
     //@RateLimiter(name = "default") // define qnts requisições o método pode receber
     @Bulkhead(name = "default")
-    public String fooBar(){
+    public String fooBar() {
         log.info("Request to foo-bar is received");
 //        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
 //        return response.getBody();
         return "Foo-Bar";
     }
 
-    public String fallbackMethod(Exception ex){ // deve ter um exception
+    public String fallbackMethod(Exception ex) { // deve ter um exception
         log.info("Fallback called");
         return "Fallback Method Foo-Bar";
     }
