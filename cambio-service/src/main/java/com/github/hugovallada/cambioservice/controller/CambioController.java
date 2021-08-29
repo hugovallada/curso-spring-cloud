db9a820d00c599bc96dd5df2943a225084cc4be4
hugovallada <valladahugo@gmail.com>
2021-08-29 13:59:54 -0300

    Adiciona o OpenAPI e OpenAPI UI para o cambio service

--- cambio-service/src/main/java/com/github/hugovallada/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/github/hugovallada/cambioservice/controller/CambioController.java
@@ -2,11 +2,11 @@ package com.github.hugovallada.cambioservice.controller;
 
 import com.github.hugovallada.cambioservice.model.Cambio;
 import com.github.hugovallada.cambioservice.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio endpoint")
 @Log4j2
 @RestController
 @RequestMapping("/cambio-service")
@@ -26,16 +27,17 @@ public class CambioController {
 
     private CambioRepository repository;
 
+    @Operation(description = "Get cambio from currency")
     @GetMapping("{amount}/{from}/{to}")
     @SneakyThrows
     public Cambio getCambio(
             @PathVariable BigDecimal amount,
             @PathVariable String from,
             @PathVariable String to
-    ){
+    ) {
         log.info("Cambio API received a request to get the cambio");
         var cambio = repository.findByFromAndTo(from, to);
-        if(cambio == null) throw new Exception("Currency unsupported");
+        if (cambio == null) throw new Exception("Currency unsupported");
 
         var port = environment.getProperty("local.server.port");
         cambio.setEnvironment(port);
@@ -45,7 +47,7 @@ public class CambioController {
 
         cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
 
-        return new Cambio(1L, from, to, cambio.getConversionFactor(), cambio.getConvertedValue() , cambio.getEnvironment());
+        return new Cambio(1L, from, to, cambio.getConversionFactor(), cambio.getConvertedValue(), cambio.getEnvironment());
     }
 
 
